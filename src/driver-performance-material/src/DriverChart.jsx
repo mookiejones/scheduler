e153bdfcd27f7195a3150d5094994b85a1779d65
mookiejones <charles.heath.berman@gmail.com>
2018-07-04 06:48:55 -0400

    creating magna-header

---
+++ src/driver-performance-material/src/DriverChart.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import * as d3 from 'react-d3-basic'
+export default class DriverChart extends Component {
+
+    render(){
+        return (
+        <div></div>)
+    }
+}
---
+++ src/magna-header/src/index.ts
@@ -0,0 +1,2 @@
+export * from "./ConnectionIndicator";
+export * from './MagnaHeader';
